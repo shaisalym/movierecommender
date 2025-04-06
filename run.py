33b05726c267ed9931214cb4d3d72d3f72cc95da
Shai Saleem <shaisalym@Shais-MacBook-Pro.local>
2025-04-06 09:52:12 +0500

    bug

--- run.py
+++ run.py
@@ -15,4 +15,4 @@ app = create_app()
 
 if __name__ == '__main__':
     # Use environment variables for production (host = 0.0.0.0, port = $PORT)
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5001)), debug=False)
