b7b6e04e99afad9e7f580a228d3967cc0ec5335b
Shai Saleem <shaisalym@Shais-MacBook-Pro.local>
2025-04-06 09:27:48 +0500

    run.py update

--- run.py
+++ run.py
@@ -1,8 +1,18 @@
+
+# from app import create_app
+
+# app = create_app()
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
 import os
 from app import create_app
 
+# Create the app using the factory function
 app = create_app()
 
 if __name__ == '__main__':
+    # Use environment variables for production (host = 0.0.0.0, port = $PORT)
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
-
